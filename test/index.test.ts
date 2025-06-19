2ad613edbb0dd880166dc23e363f95e183d6dc86
mbp <3522003+cdaein@users.noreply.github.com>
2025-06-19 17:50:50 +0900

    fix: new test for assemble()

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,12 +7,42 @@ import {
   afterEach,
   MockInstance,
 } from "vitest";
-import { isHangul } from "../src";
+import { assemble, isHangul } from "../src";
 
-const warnMsg = (input: string) =>
-  `Only the first character ("${input}") will be processed.`;
+describe("assemble()", () => {
+  test("assembles multiple Hangul jaso combinations", () => {
+    expect(assemble(["ㅁ", "ㅏ", "ㄹ", "ㄱ"])).toBe("맑");
+    expect(assemble(["ㄱ", "ㅗ", "ㅏ"])).toBe("과");
+  });
+
+  test("returns individual jaso if they cannot be assembled", () => {
+    expect(assemble(["ㅁ", "ㅁ", "ㅏ"])).toBe("ㅁ마");
+    expect(assemble(["ㅁ", "ㅜ", "ㄹ", "ㄷ"])).toBe("물ㄷ");
+    expect(assemble(["ㅂ", "ㅂ", "ㅏ"])).toBe("ㅂ바");
+  });
+
+  test("returns non-Hangul as is", () => {
+    expect(assemble(["a", "모", "ㅁ"])).toBe("a몸");
+    expect(assemble(["ㅁ", "ㅏ", "3", "ㅗ"])).toBe("마3ㅗ");
+  });
+
+  test("returns space, empty string and punctuations as is", () => {
+    expect(assemble(["ㅎ", "ㅏ", " ", "하"])).toBe("하 하");
+    expect(assemble(["ㅁ", "ㅏ", "!", "?"])).toBe("마!?");
+    expect(assemble(["ㅁ", "ㅏ", "", "?"])).toBe("마?");
+  });
+
+  test("handles multiple characters in an array element", () => {
+    expect(assemble(["학교", " ", "가자", "!"])).toBe("학교 가자!");
+    expect(assemble(["java", " ", "script", "좋아 "])).toBe("java script좋아 ");
+    expect(assemble(["ㅁ", "ㅏ", "   ", "?"])).toBe("마   ?");
+  });
+});
 
 describe("isHangul()", () => {
+  const warnMsg = (input: string) =>
+    `Only the first character ("${input}") will be processed.`;
+
   let consoleWarnSpy: MockInstance;
   beforeEach(() => {
     consoleWarnSpy = vi.spyOn(console, "warn");
@@ -25,18 +55,15 @@ describe("isHangul()", () => {
   test("returns true for a complete Hangul syllable", () => {
     expect(isHangul("가")).toBe(true);
     expect(isHangul("맑")).toBe(true);
-    expect(isHangul("토")).toBe(true);
   });
 
   test("returns true for a Hangul jamo", () => {
     expect(isHangul("ㄱ")).toBe(true);
     expect(isHangul("ㅛ")).toBe(true);
     expect(isHangul("ㄲ")).toBe(true);
-    expect(isHangul("ㅏ")).toBe(true);
   });
 
   test("returns false for a non-Hangul character", () => {
-    expect(isHangul("a")).toBe(false);
     expect(isHangul("z")).toBe(false);
     expect(isHangul("3")).toBe(false);
     expect(isHangul("?")).toBe(false);
@@ -49,7 +76,7 @@ describe("isHangul()", () => {
     expect(consoleWarnSpy).toHaveBeenCalledWith(warnMsg(input[0]));
   });
 
-  test("rleturns false after warning if first character is non-Hangul", () => {
+  test("returns false after warning if first character is non-Hangul", () => {
     const input = "hey헤이";
     expect(isHangul(input)).toBe(false);
     expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
