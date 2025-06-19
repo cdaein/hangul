0ba54bbf706bd1c7a39644d8153ec5672164f8ed
mbp <3522003+cdaein@users.noreply.github.com>
2025-06-19 17:24:04 +0900

    fix: add test for isHangul()

---
+++ test/index.test.ts
@@ -0,0 +1,58 @@
+import {
+  describe,
+  expect,
+  test,
+  vi,
+  beforeEach,
+  afterEach,
+  MockInstance,
+} from "vitest";
+import { isHangul } from "../src";
+
+const warnMsg = (input: string) =>
+  `Only the first character ("${input}") will be processed.`;
+
+describe("isHangul()", () => {
+  let consoleWarnSpy: MockInstance;
+  beforeEach(() => {
+    consoleWarnSpy = vi.spyOn(console, "warn");
+  });
+  // prevent test pollution and ensure tests are isolated.
+  afterEach(() => {
+    consoleWarnSpy.mockRestore();
+  });
+
+  test("returns true for a complete Hangul syllable", () => {
+    expect(isHangul("가")).toBe(true);
+    expect(isHangul("맑")).toBe(true);
+    expect(isHangul("토")).toBe(true);
+  });
+
+  test("returns true for a Hangul jamo", () => {
+    expect(isHangul("ㄱ")).toBe(true);
+    expect(isHangul("ㅛ")).toBe(true);
+    expect(isHangul("ㄲ")).toBe(true);
+    expect(isHangul("ㅏ")).toBe(true);
+  });
+
+  test("returns false for a non-Hangul character", () => {
+    expect(isHangul("a")).toBe(false);
+    expect(isHangul("z")).toBe(false);
+    expect(isHangul("3")).toBe(false);
+    expect(isHangul("?")).toBe(false);
+  });
+
+  test("returns true after warning if first character is Hangul", () => {
+    const input = "하hello";
+    expect(isHangul(input)).toBe(true);
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+    expect(consoleWarnSpy).toHaveBeenCalledWith(warnMsg(input[0]));
+  });
+
+  test("rleturns false after warning if first character is non-Hangul", () => {
+    const input = "hey헤이";
+    expect(isHangul(input)).toBe(false);
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+    expect(consoleWarnSpy).toHaveBeenCalledWith(warnMsg(input[0]));
+  });
+});
