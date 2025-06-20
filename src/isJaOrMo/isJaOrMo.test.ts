5808e359bcea89f51e36e74f27707d7f0ca8ea75
mbp <3522003+cdaein@users.noreply.github.com>
2025-06-20 13:01:49 +0900

    fix: more tests for isJaOrMo()

--- src/isJaOrMo/isJaOrMo.test.ts
+++ src/isJaOrMo/isJaOrMo.test.ts
@@ -9,8 +9,6 @@ import {
 } from "vitest";
 import { isJaOrMo } from "./isJaOrMo";
 
-// TODO: add more tests
-
 describe("isJaOrMo()", () => {
   const warnMsg = (input: string) =>
     `Only the first character ("${input}") will be processed.`;
@@ -25,8 +23,11 @@ describe("isJaOrMo()", () => {
   });
 
   test('returns "ja" for consonant', () => {
-    //
+    expect(isJaOrMo("ㅂ")).toBe("ja");
+    expect(isJaOrMo("ㄲ")).toBe("ja");
+    expect(isJaOrMo("ㄳ")).toBe("ja");
   });
+
   test('returns "ja" after warning if first character is consonant', () => {
     const input = "ㅎhello";
     expect(isJaOrMo(input)).toBe("ja");
@@ -35,8 +36,10 @@ describe("isJaOrMo()", () => {
   });
 
   test('returns "mo" for vowel', () => {
-    //
+    expect(isJaOrMo("ㅗ")).toBe("mo");
+    expect(isJaOrMo("ㅞ")).toBe("mo");
   });
+
   test('returns "mo" after warning if first character is vowel', () => {
     const input = "ㅟhello";
     expect(isJaOrMo(input)).toBe("mo");
@@ -45,10 +48,13 @@ describe("isJaOrMo()", () => {
   });
 
   test('returns "syllable" for full Hangul syllable', () => {
-    //
+    expect(isJaOrMo("한")).toBe("syllable");
+    expect(isJaOrMo("뷃")).toBe("syllable");
   });
 
   test('returns "other" for non-Hangul', () => {
-    //
+    expect(isJaOrMo("a")).toBe("other");
+    expect(isJaOrMo(" ")).toBe("other");
+    expect(isJaOrMo("$")).toBe("other");
   });
 });
