7f05df7ed3f3bc6fb9a1d2026e77400c62a08075
mbp <3522003+cdaein@users.noreply.github.com>
2025-06-20 12:26:53 +0900

    wip: disassemble

--- src/index.ts
+++ src/index.ts
@@ -22,18 +22,69 @@ import {
   isHangulSyllable,
 } from "./isHangul/isHangul";
 
+export interface DisassembledCharacter {
+  choseong: string;
+  jungseong: string;
+  jongseong: string;
+}
+
 /**
+ * Disassemble Hangul syllable(s) into inital(choseong), medial(jungseong), and final(jongseong).
+ * If input string is not a full Hangul syllable, it will be returned as either choseong(consonant) or jungseong(vowel).
+ * Non-Hangul characters will be returned as is.
  *
+ * @param str - string to disassemble
  *
  * @example
  * ```ts
+ * disassemble("ㅁ") // [{ choseong: "ㅁ", jungseong: "", jongseong: "" }]
+ * disassemble("ㅗ")) // [{ choseong: "", jungseong: "ㅗ", jongseong: "" }]
+ * disassemble("몫") // [{ choseong: "ㅁ", jungseong: "ㅗ", jongseong: "ㄱㅅ" }]
+ * disassemble("꽈") // [{ choseong: "ㄲ", jungseong: "ㅗㅏ", jongseong: "" }]
+ * disassemble("와yo") // [{ choseong: "ㅇ", jungseong: "ㅗㅏ", jongseong: "" }, "y", "o"]
  * ```
  */
-
-
-    } else {
+export const disassemble = (
+  str: string,
+  // opts = {
+  //   completeOnly: false,
+  // },
+): (string | DisassembledCharacter)[] => {
+  return Array.from(str).map((ch) => {
+    // 1. non-Hangul - return as is
+    if (!isHangul(ch)) {
+      return ch;
     }
 
+    const disassembled = disassembleCompleteCharacter(ch) as
+      | DisassembledCharacter
+      | undefined;
+
+    if (disassembled) {
+      // 2. complete Hangul syllable - break into cho-, jung-, jongseong
+      return disassembled;
+    } else {
+      // 3. Hangul jaso
+      const jamo = isJaOrMo(ch);
+      // FIX: disassemble complex ja(ㅅ타벇) or mo(ㅟ)
+      if (jamo === "ja") {
+        // if jaum, consider it as choseong
+        return {
+          choseong: ch,
+          jungseong: "",
+          jongseong: "",
+        };
+      } else if (jamo === "mo") {
+        return {
+          choseong: "",
+          jungseong: eshDisassemble(ch),
+          jongseong: "",
+        };
+      }
+    }
+
+    return ch;
+  });
 };
 
 /**
