fed232edc2c98242cff61d310ee39517d87a4d50
mbp <3522003+cdaein@users.noreply.github.com>
2025-06-22 14:04:32 +0900

    fix: disassemble() jsdoc

--- src/disassemble/disassemble.ts
+++ src/disassemble/disassemble.ts
@@ -12,11 +12,11 @@ export interface DisassembledCharacter {
 }
 
 /**
- * Disassemble Hangul syllable(s) into inital(choseong), medial(jungseong), and final(jongseong).
+ * Disassemble Hangul syllable(s) into `[ { choseong, jungseong, jongseong }, ... ]`
  * If input string is not a full Hangul syllable, it will be returned as either choseong(consonant) or jungseong(vowel).
- * It cannot disassemble Hangul characters from Jamo, Jamo Extended A & B ranges. They will be returned as is.
+ * Hangul characters from Jamo, Jamo Extended A & B ranges are not disassembled and returned as is.
  * This is due to the current reliance on `es-hangul`, which does not disassemble Jamo range.
- * Non-Hangul characters will be returned as is.
+ * Non-Hangul characters are returned as is.
  *
  * @param str - string to disassemble
  *
@@ -24,6 +24,7 @@ export interface DisassembledCharacter {
  * ```ts
  * disassemble("ㅁ") // [{ choseong: "ㅁ", jungseong: "", jongseong: "" }]
  * disassemble("ㅗ")) // [{ choseong: "", jungseong: "ㅗ", jongseong: "" }]
+ * disassemble("ㄼ") // [{ choseong: "ㄹㅂ", jungseong: "", jongseong: "" }]
  * disassemble("몫") // [{ choseong: "ㅁ", jungseong: "ㅗ", jongseong: "ㄱㅅ" }]
  * disassemble("꽈") // [{ choseong: "ㄲ", jungseong: "ㅗㅏ", jongseong: "" }]
  * disassemble("와yo") // [{ choseong: "ㅇ", jungseong: "ㅗㅏ", jongseong: "" }, "y", "o"]
