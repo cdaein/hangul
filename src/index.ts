ea9495e17203704ba23878c93a1c6d33017c4393
mbp <3522003+cdaein@users.noreply.github.com>
2025-07-20 20:26:39 +0900

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./consonants";
 export * from "./assemble";
 export * from "./canBeJongseong";
 export * from "./disassemble";
+export * from "./disassembleToJamoString";
 export * from "./hangul-syllable";
 export * from "./isHangul";
 export * from "./isJaOrMo";
